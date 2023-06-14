b297488b5e939ca0245d6abfe2f10ce21df45556
Daniel Grzegozeski <kmkzplays@outlook.com>
2023-06-14 15:48:49 -0300

    Atualizacao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TelaLoginAdmin form = new TelaLoginAdmin();
+        form.setContentPane(form.JPanelUm);
+        form.setVisible(true);
+        form.setSize(400,500);
     }
 }
--- src/TelaLoginAdmin.java
+++ src/TelaLoginAdmin.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class TelaLoginAdmin {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TelaLoginAdmin extends JFrame {
+    public JPanel JPanelUm;
+    public JTextField textUsuario;
+    public JTextField textSenha;
+    private JButton loginButton;
+    private JButton limparButton;
+
+    private String usuario;
+
+    private String senha;
+
+public TelaLoginAdmin() {
+
+
+
+    loginButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+
+            usuario = textUsuario.getText();
+            senha = textSenha.getText();
+
+            if(usuario.equals("admin") && senha.equals("admin")){
+
+                JOptionPane.showMessageDialog(null,"logado com sucesso");
+
+
+            }else{
+                JOptionPane.showMessageDialog(null,"Senha ou Usuário Incorreto");
+            }
+
+            }
+
+    });
+    limparButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            textUsuario.setText("");
+            textSenha.setText("");
+        }
+    });
+}
 }
